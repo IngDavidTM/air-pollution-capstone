8d81869712eff93f5ede859e4cb9110cecbc47c5
IngDavidTM <david-1011@hotmail.com>
2022-11-21 22:44:39 -0500

    filter by country

--- src/pages/Country.js
+++ src/pages/Country.js
@@ -1,12 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-const Country = () => (
-  <nav>
-    <Link to="/">{'< Region'}</Link>
-    <h2>C</h2>
-    <i className="fa-solid fa-microphone" />
-    <i className="fa-solid fa-gear" />
-  </nav>
-);
+const Country = () => {
+  const { info } = useParams();
+  const countriesArr = useSelector((state) => state.countries);
+  const result = countriesArr.filter((country) => country.cca2 === info.toUpperCase());
+  return (
+    <nav>
+      <Link to="/">{'< Region'}</Link>
+      <h2>{result[0].latlng[0]}</h2>
+      <h2>{result[0].latlng[1]}</h2>
+      <i className="fa-solid fa-microphone" />
+      <i className="fa-solid fa-gear" />
+    </nav>
+  );
+};
 
 export default Country;
