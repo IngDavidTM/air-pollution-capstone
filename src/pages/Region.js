1b3f1c3d1efe66a46f006911a5c28991500030ab
IngDavidTM <david-1011@hotmail.com>
2022-11-22 16:37:08 -0500

    add style to region page

--- src/pages/Region.js
+++ src/pages/Region.js
@@ -3,6 +3,7 @@ import {
 } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Country from './Country';
+import '../stylesheet/Region.css';
 
 const Region = () => {
   const { country } = useParams();
@@ -11,24 +12,32 @@ const Region = () => {
   return (
     <>
       <div>
-        <nav>
-          <Link to="/">{'< Region'}</Link>
-          <h2>countries</h2>
+        <nav className="navRegion">
+          <Link to="/">
+            <i className="fa-solid fa-chevron-left" />
+            REGION
+          </Link>
+          <h2>Countries</h2>
           <i className="fa-solid fa-microphone" />
           <i className="fa-solid fa-gear" />
         </nav>
         <div className="countries">
-          <h3>{country}</h3>
-          <img alt={country} src={`https://raw.githubusercontent.com/Ginohmk/worldMaps/main/maps/${country.toLowerCase()}/vector.svg`} />
-          {countriesArr.map((country) => (
-            <NavLink key={country.cca2} to={`${country.cca2.toLowerCase()}/${country.latlng[0]}/${country.latlng[0]}/${country.name.common}`}>
-              <div>
-                <img alt={country.cca2} src={`https://raw.githubusercontent.com/Ginohmk/worldMaps/main/maps/${country.cca2.toLowerCase()}/vector.svg`} />
-                <h3>{country.name.common}</h3>
-                <p>{country.population}</p>
-              </div>
-            </NavLink>
-          ))}
+          <header>
+            <img alt={country} src={`https://raw.githubusercontent.com/Ginohmk/worldMaps/main/maps/${country.toLowerCase()}/vector.svg`} />
+            <h3 className="countryName">{country}</h3>
+          </header>
+          <h4>POPULATION</h4>
+          <div className="countriesContainer">
+            {countriesArr.map((country) => (
+              <NavLink key={country.cca2} to={`${country.cca2.toLowerCase()}/${country.latlng[0]}/${country.latlng[0]}/${country.name.common}`}>
+                <div className="itemCountry">
+                  <img alt={country.cca2} src={`https://raw.githubusercontent.com/Ginohmk/worldMaps/main/maps/${country.cca2.toLowerCase()}/vector.svg`} />
+                  <h3>{country.name.common.toUpperCase()}</h3>
+                  <p>{country.population}</p>
+                </div>
+              </NavLink>
+            ))}
+          </div>
         </div>
       </div>
       <Routes>
