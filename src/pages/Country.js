aee3d28cea00e1f83d533b6a885e5b5bb460f563
IngDavidTM <david-1011@hotmail.com>
2022-11-22 19:00:16 -0500

    change chart

--- src/pages/Country.js
+++ src/pages/Country.js
@@ -3,25 +3,15 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import {
   Chart as ChartJS,
-  RadialLinearScale,
-  PointElement,
-  LineElement,
-  Filler,
+  ArcElement,
   Tooltip,
   Legend,
 } from 'chart.js';
-import { Radar } from 'react-chartjs-2';
+import { Doughnut } from 'react-chartjs-2';
 import { getInfo } from '../redux/country/country';
 import '../stylesheet/Country.css';
 
-ChartJS.register(
-  RadialLinearScale,
-  PointElement,
-  LineElement,
-  Filler,
-  Tooltip,
-  Legend,
-);
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Country = () => {
   const {
@@ -37,7 +27,7 @@ const Country = () => {
     labels: ['CO', 'NH3', 'NO', 'NO2', 'O3', 'PM2_5', 'PM10', 'SO2'],
     datasets: [
       {
-        label: 'COMPONENTS',
+        label: 'Quantity',
         data: [
           infoArr.co,
           infoArr.nh3,
@@ -48,15 +38,33 @@ const Country = () => {
           infoArr.pm10,
           infoArr.so2,
         ],
-        backgroundColor: '#ffffffd2',
-        borderColor: '#ffffffd2',
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(94, 94, 94, 0.2)',
+          'rgba(0, 0, 0, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(94, 94, 94, 1)',
+          'rgba(0, 0, 0, 1)',
+        ],
         borderWidth: 1,
       },
     ],
   };
 
   function ChartRadar() {
-    return <Radar data={data} />;
+    return <Doughnut data={data} />;
   }
 
   return (
@@ -85,6 +93,7 @@ const Country = () => {
           <p>{`SO2: ${infoArr.so2}`}</p>
         </div>
         <div className="chart">
+          <h2>Doughnut Chart</h2>
           <ChartRadar />
         </div>
       </div>
